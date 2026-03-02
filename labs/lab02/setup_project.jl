60bfb7c6ba512cc10bd7c200d43be0fc3877b4a3
neve7mind <neve07mind@gmail.com>
2026-03-02 19:00:04 +0300

    feat(lab02): report done

---
+++ labs/lab02/setup_project.jl
@@ -0,0 +1,12 @@
+##!/usr/bin/env julia
+## setup_project.jl
+
+using Pkg
+Pkg.add("DrWatson")
+using DrWatson
+
+## Создание проекта
+project_name = "project"
+initialize_project(project_name; authors="ivmurashov", git=false)
+println("✅ Проект создан: ", project_name)
+println("� Перейдите в директорию: cd ", project_name)
